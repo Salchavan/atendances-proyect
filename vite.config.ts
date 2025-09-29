87c689a384406d67c9e446adebaf8a53ea14eaaa
Salvador Castellanos <salvadorcastellanosposse@gmail.com>
2025-09-29 10:42:54 -0300

    chore: update dependencies and improve Vite configuration
    - Updated MUI and Tailwind CSS dependencies to latest versions.
    - Added new dependencies for Emotion and Vite React plugin.
    - Updated react-router and rsuite to newer versions.
    - Removed unused dependencies from package.json.
    - Added school logo image to assets.
    - Changed Vite configuration to use the standard React plugin and added deduplication and optimization for React and React DOM.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
@@ -8,5 +8,11 @@ export default defineConfig({
   base: '/atendances-proyect/',
   build: {
     sourcemap: false,
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
   },
 });
